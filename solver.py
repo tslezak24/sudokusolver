3690c32f9a40382453c39d3392bf15d8bcfb62b6
tslezak24 <tmslezak24@gmail.com>
2024-04-03 07:31:55 -0500

    Basic logic in place to complete through manual filling out of array

--- solver.py
+++ solver.py
@@ -1,10 +1,29 @@
 from collections import defaultdict
 class Solver: 
+    def find_open_space(board):
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == ".":
+                    return (row, col)
+        return None
+        
     def solve(board):
-        if Solver.verify_currently_valid(board):
-            return board
-        else:
-            raise Exception("Unsolvable")
+        space = Solver.find_open_space(board)
+        
+        if space is None and Solver.verify_currently_valid(board):
+            return True
+        
+        for i in range(1, 10):
+            if Solver.verify_currently_valid(board):
+                board[space[0]][space[1]] = f"{i}"
+                
+                if Solver.solve(board):
+                    return True 
+                
+                board[space[0]][space[1]] = "."
+        
+        return False
+        
     
     def verify_currently_valid(board):
         rows = defaultdict(set)
